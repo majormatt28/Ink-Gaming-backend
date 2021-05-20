129138e53fc393dde7248d3ef78af895ec4643fe
Matthew <elijah2895@hotmail.com>
2021-05-20 17:24:11 -0400

    worked on users table some more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
         user = User.find(params[:id])
         render json: user
     end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+    end
+
+    private
+    
+    def user_params
+        params.permit(:username, :password, :email)
+    end
 end
