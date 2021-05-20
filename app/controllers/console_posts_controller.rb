ad7cdeb466b105499c1b14365be66ca0713881a3
Matthew <elijah2895@hotmail.com>
2021-05-20 16:30:43 -0400

    fixed up some of the contollers

--- app/controllers/console_posts_controller.rb
+++ app/controllers/console_posts_controller.rb
@@ -1,3 +1,11 @@
-class TagPostsController < ApplicationController
-    
+class ConsolePostsController < ApplicationController
+    def index
+        tags = ConsolePost.all
+        render json: tags
+    end
+
+    def show
+        tag = ConsolePost.find(params[:id])
+        render json: tag
+    end
 end
